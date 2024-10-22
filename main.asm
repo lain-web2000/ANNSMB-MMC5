13f2741d413155afe8f6da62365160d79ab72582
Lain <51301652+lain-web2000@users.noreply.github.com>
2024-10-22 01:29:10 +0100

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,11 @@
+.segment "NSMMAIN"
+	.incbin "NSMMAIN.bin"
+
+.segment "NSMDATA2"
+	.incbin "NSMDATA2.bin"
+
+.segment "NSMDATA3"
+	.incbin "NSMDATA3.bin"
+
+.segment "NSMDATA4"
+	.incbin "NSMDATA4.bin"
